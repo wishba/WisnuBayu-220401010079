85633367e5a13fa89843cb6a1579c9e1d9e75c19
wishba <wisnubayu34@gmail.com>
2024-05-22 19:56:43 +0700

    fix to accept string

--- includes/update.php
+++ includes/update.php
@@ -6,7 +6,7 @@ $id = $_POST['id'];
 $subject = $_POST['subject'];
 $content = $_POST['content'];
 
-$sql = "UPDATE posts SET subject=$subject, content=$content WHERE id=$id";
+$sql = "UPDATE posts SET subject='$subject', content='$content' WHERE id=$id";
 mysqli_query($conn, $sql);
 
 header("Location: ../index.php");
