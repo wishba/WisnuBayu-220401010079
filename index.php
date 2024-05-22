29aaa8d102facb9df528c8cc1cf3f9c9010a6780
wishba <wisnubayu34@gmail.com>
2024-05-22 17:24:17 +0700

    simple styling

--- index.php
+++ index.php
@@ -8,13 +8,18 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
+<h1>Personal Journal App</h1>
+
 <form action="includes/insert.php" method="POST">
-  <input type="text" name="subject" placeholder="subject">
-  <input type="text" name="content" placeholder="content">
-  <button type="submit" name="submit">button</button>
+  <textarea  name="subject" placeholder="title"></textarea>
+  <br>
+  <textarea name="content" placeholder="content" rows="10"></textarea>
+  <br>
+  <button type="submit" name="submit">Save Journal</button>
 </form>
 
 <?php 
@@ -24,13 +29,13 @@
 
   if ($resultCheck > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
-      echo $row['subject'] . "<br>";
-      echo $row['content'] . "<br>";
+      echo "<div>";
+      echo "<h2>" . $row['subject'] . "</h2>";
+      echo "<p>" . $row['content'] . "</p>";
+      echo "<hr>";
+      echo "</div>";
     }
   }
-
-  // $sql = "INSERT INTO posts (subject, content, date) VALUES ('ini subjek', 'id aliquet risus feugiat in ante metus dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia at quis risus sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum non consectetur a erat nam at lectus urna duis convallis convallis tellus id interdum velit laoreet id donec ultrices tincidunt arcu non sodales neque sodales ut etiam sit amet nisl purus in mollis nunc sed id semper risus in hendrerit gravida rutrum quisque non tellus orci ac auctor augue mauris augue neque gravida in fermentum et sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl vel pretium lectus quam id leo in vitae turpis massa sed elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis urna id volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim convallis aenean et tortor at risus viverra adipiscing at in tellus integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit amet porttitor eget dolor morbi non arcu risus quis varius quam quisque id diam vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam', '2024-01-01 01:01:01');";
-  // mysqli_query($conn, $sql);
 ?>
 
 </body>
