063daf2146f516ac06f0fec242a0742f58f02d3b
wishba <wisnubayu34@gmail.com>
2024-05-22 15:52:12 +0700

    first commit

---
+++ includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServerName = "localhost";
+$dbUsername= "root";
+$dbPassword = "";
+$dbName = "demo";
+
+$conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+  include_once 'includes/dbh.inc.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+<?php 
+  $sql = "SELECT * FROM posts;";
+  $result = mysqli_query($conn, $sql);
+  $resultCheck = mysqli_num_rows($result);
+
+  if ($resultCheck > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+      echo $row['subject'] . "<br>";
+    }
+  }
+?>
+
+</body>
+</html>
